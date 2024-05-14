48c51daef4232256c450600b133dab0754963c5f
User <beletskiypr@mer.ci.nsu.ru>
2024-05-14 21:55:26 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Servers servers = new Servers();
+            servers.Add("http://rrrr");
+            servers.Add("https://rrrr");
+            servers.Add("avb://rrrr");
+
+            List<string> serv = servers.GetServer();
+            foreach (string s in serv)
+            {
+                Console.WriteLine(s);
+            }
+
+            serv = servers.GetServer("http");
+            foreach (string s in serv)
+            {
+                Console.WriteLine(s);
+            }
+
+            serv = servers.GetServer("https");
+            foreach (string s in serv)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ Servers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Servers
+    {
+        static Lazy<Servers> servers = new();
+        public static List<string> listServer = new();
+        public static Servers Istance
+        {
+            get
+            {
+                return servers.Value;
+            }
+        }
+
+        public bool Add(string server)
+        {
+            if (server.StartsWith("http") || server.StartsWith("https"))
+            {
+                listServer.Add(server);
+                return true;
+            }
+            return false;
+        }
+        public List<string> GetServer(string parameter)
+        {
+            switch (parameter.ToLower())
+            {
+                case "http":
+                    return GetServers(0);
+                case "https":
+                    return GetServers(1);
+                default: return listServer;
+            }
+        }
+        protected List<string> GetServers(int parameter)
+        {
+            List<string> list = new();
+            foreach (var server in listServer)
+            {
+                if (parameter == 0 && server.ToString().Substring(0,5) != "https") list.Add(server);
+                if (parameter == 1 && server.ToString().Substring(0, 5) == "https") list.Add(server);
+            }
+            return list;
+        }
+        public List<string> GetServer()
+        {
+            return listServer;
+        }
+
+    }
+}
